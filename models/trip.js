3bf05e4749fccf109d1f947b04a472bc0173d147
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-09-09 12:16:13 +0530

    stored the secret in .env file

--- models/trip.js
+++ models/trip.js
@@ -17,11 +17,8 @@ module.exports = (sequelize, DataTypes) => {
     starting_date: DataTypes.DATE,
     category: DataTypes.STRING,
     no_of_days: DataTypes.INTEGER,
-    route_id: DataTypes.INTEGER,
-    adult_count: DataTypes.INTEGER,
-    children_count: DataTypes.INTEGER,
     starting_place: DataTypes.STRING,
-    destination: DataTypes.STRING
+    user_id: DataTypes.INTEGER,
   }, {
     sequelize,
     modelName: 'trip',
