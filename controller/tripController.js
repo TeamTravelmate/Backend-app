fb3fad35ecc3d304a011fe012c60b7550c831ac9
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-09-10 20:45:59 +0530

    Insert, get and update functions for budget

--- controller/tripController.js
+++ controller/tripController.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
 const {
-  trip
+  trip: tripModel,
+  expense: expenseModel,
+  budget: budgetModel,
+  budget_category: budget_categoryModel,
+  sequelize
 } = require('../models');
 const validateUser = require('../middleware/validateUser');
+const {
+  getCategoryId
+} = require('../helpers/categoryCheck');
+const {
+  capitalizeFirst
+} = require('../helpers/capitalizeFirstLetter');
 
+expenseModel.belongsTo(budget_categoryModel, {
+  foreignKey: 'category'
+});
 
 // $baseUrl/trip gives all public trips
 router.get('/', async (req, res) => {
   try {
-    const trips = await trip.findAll({
+    const trips = await tripModel.findAll({
       where: {
         category: "Public"
       }
@@ -25,7 +38,7 @@ router.get('/', async (req, res) => {
 // $baseUrl/trip/myTrips gives all trips of a user
 router.get('/myTrips', validateUser, async (req, res) => {
   try {
-    const trips = await trip.findAll({
+    const trips = await tripModel.findAll({
       where: {
         user_id: req.user.userId
       }
@@ -43,7 +56,7 @@ router.get('/myTrips', validateUser, async (req, res) => {
 router.get('/:id', async (req, res) => {
   console.log(req.params);
   try {
-    const trips = await trip.findByPk(req.params.id);
+    const trips = await tripModel.findByPk(req.params.id);
     res.status(200).send(trips);
   } catch (err) {
     console.log(err);
@@ -58,7 +71,7 @@ router.put('/:id', validateUser, async (req, res) => {
   console.log(req.body);
   console.log(req.user.userId);
   try {
-    const trips = await trip.update(req.body, {
+    const trips = await tripModel.update(req.body, {
       where: {
         id: req.params.id,
         user_id: req.user.userId
@@ -92,10 +105,10 @@ router.post('/', validateUser, async (req, res) => {
     } = req.body;
 
     const userId = req.user.userId;
-    category = category.toLowerCase();
-    category = category.charAt(0).toUpperCase() + category.slice(1);
 
-    const newTrip = await trip.create({
+    category = capitalizeFirst(category);
+
+    const newTrip = await tripModel.create({
       starting_date: startDate,
       category: category,
       no_of_days: numberOfDays,
@@ -115,6 +128,180 @@ router.post('/', validateUser, async (req, res) => {
   }
 });
 
+//***$baseurl/trip/budget/:tripId***
+
+//post - inserting/creating a budget
+router.post('/budget/:tripId', validateUser, async (req, res) => {
+  try {
+    const {
+      tripId
+    } = req.params;
+    const {
+      expenses
+    } = req.body;
+    const userId = req.user.userId;
+    let budgetAmount = 0;
+    const newExpenses = [];
+
+    const newBudget = await budgetModel.create({
+      budget_amount: budgetAmount,
+      tripID: tripId,
+    });
+
+    for (const tripExpense of expenses) {
+      let {
+        category,
+        expense_name,
+        amount
+      } = tripExpense;
+      budgetAmount += amount;
+
+      category = capitalizeFirst(category);
+      const categoryId = await getCategoryId(category);
+
+
+      const newExpense = await expenseModel.create({
+        category: categoryId,
+        expense_name: expense_name,
+        amount: amount,
+        budget_id: newBudget.id,
+        userID: userId
+      });
+
+      newExpenses.push(newExpense);
+    }
+
+    newBudget.amount = budgetAmount;
+    await newBudget.save();
+
+
+    res.status(201).send({
+      message: "Budget created successfully",
+      budget: newBudget,
+      expenses: newExpenses
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server error"
+    });
+  }
+});
+
+//get - getting a budget
+router.get('/budget/:tripId', async (req, res) => {
+  const {
+    tripId
+  } = req.params;
+
+  try {
+    const newBudget = await budgetModel.findOne({
+      where: {
+        tripID: tripId,
+      },
+      attributes: ['id', 'amount'],
+    })
+    let expenses = await expenseModel.findAll({
+      where: {
+        budget_id: newBudget.id,
+      },
+      attributes: ['id', 'expense_name', 'amount'],
+      include: [{
+        model: budget_categoryModel,
+        on: sequelize.literal('budget_category.id::text = expense.category'),
+        attributes: ['category_name'],
+        required: true,
+      }]
+    })
+    expenses = expenses.map(expense => {
+      return {
+        id: expense.id,
+        expense_name: expense.expense_name,
+        amount: expense.amount,
+        category: expense.budget_category.category_name,
+      };
+    })
+    res.status(200).send({
+      budget: newBudget,
+      expenses: expenses
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server error"
+    });
+  }
+})
+
+//put - updating a budget
+router.put('/budget/:tripId', validateUser, async (req, res) => {
+  const tripId = req.params.tripId;
+  const userId = req.user.userId;
+
+  try {
+    let {
+      budget,
+      expenses
+    } = req.body;
+
+    const newExpenses = [];
+    let budgetAmount = 0;
+
+    //drop all previous expenses
+    await expenseModel.destroy({
+      where: {
+        budget_id: budget.id,
+      }
+    });
+
+    for (const tripExpense of expenses) {
+      let {
+        id,
+        category,
+        expense_name,
+        amount
+      } = tripExpense;
+      budgetAmount += amount;
+
+      category = capitalizeFirst(category);
+      const categoryId = await getCategoryId(category);
+
+
+      const newExpense = await expenseModel.create({
+        expense_name: expense_name,
+        amount: amount,
+        category: categoryId,
+        budget_id: budget.id,
+        userID: userId
+      }, );
+
+      newExpenses.push(newExpense);
+    }
+
+    await budgetModel.update({
+      amount: budgetAmount,
+    }, {
+      where: {
+        id: budget.id,
+      }
+    });
+
+
+    res.status(201).send({
+      message: "Updated successfully",
+      budget: {
+        id: budget.id,
+        budget_amount: budgetAmount,
+      },
+      expenses: newExpenses
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server error"
+    });
+  };
+});
 
 
 //   createExpense: async (req, res) => {
