e5e223bb77e6854438aa985e14f4043881eb2d3c
Nazma10 <nazmanazar5@gmail.com>
2023-10-01 20:36:09 +0530

    add Vendor

--- controller/vendorController.js
+++ controller/vendorController.js
@@ -175,13 +175,176 @@ async function deleteProduct(req, res){
     }
 }
 
+
+
+//*** Cart table 
+//*** (Columns: product_id,quantity(by default quantity is 1),total_amount,traveler_id,vendor_id) 
+//*** foreign keys:  product_id (vendor_essential table),traveler_id(User table),vendor_id(User table)
+
 //add to cart '$baseUrl/vendor/addToCart/:id'
+async function addToCart(req, res){
+    try{
+        const productID = req.params.id;
+        const userID = req.user.userID;
+        const product = await vendor_essentialModel.findOne({
+            where: {
+                id : productID
+            }
+        })
 
-//update quantity of product when a user add to cart & remove from cart '$baseUrl/vendor/updateQuantity/:id'
+        //check product availability
+        if (product.quantity === 0){
+            res.status(404).send({
+                message: "Product not available!"
+            });
+        } else {
+            // 1. add product to cart 
+            const cart = await cartModel.create({
+                product_id: productID,
+                quantity: quantity, //1 by default
+                total_amount: product.price,
+                traveler_id: userID,
+                vendor_id: product.user_id
+            });
 
-//remove from cart '$baseUrl/vendor/removeFromCart/:id'
+            // 2. update quantity of product in vendor_essential table (quantity--)
+            const product = await vendor_essentialModel.update({
+                quantity: product.quantity - 1
+            },{
+                where: {
+                    id : productID
+                }
+            });
+
+        res.status(201).send({
+            message: "Product added to cart successfully!",
+            cart: cart
+        });
+    }
+    } catch (err){
+        console.log(err);
+        res.status(500).send({
+            message: "Server Error!"
+        });
+    }
+}
 
 //view my cart '$baseUrl/vendor/myCart'
+async function myCart(req, res){
+    try{
+        const userID = req.user.userID;
+        const cart = await cartModel.findAll({
+            where: {
+                traveler_id : userID
+            },
+            include: [{
+                model: vendor_essentialModel,
+                as: 'vendor_essential',
+                attributes: ['assential_name', 'price', 'quantity', 'seller_name', 'address']
+            }]
+        })
+
+        res.status(200).send(cart);
+    } catch (err){
+        console.log(err);
+        res.status(500).send({
+            message: "Server Error!"
+        });
+    }
+}
+
+//update quantity of the product in cart '$baseUrl/vendor/updateQuantityCart/:id'
+async function updateQuantityCart(req, res){
+    try{
+        const productID = req.params.id;
+        const userID = req.user.userID;
+        const quantity = req.body.quantity;
+        const product = await vendor_essentialModel.findOne({
+            where: {
+                id : productID
+            }
+        })
+
+        //check product availability
+        if (product.quantity === 0){
+            res.status(404).send({
+                message: "Product not available!"
+            });
+        } else {
+            // 1. update quantity of product in cart 
+            const cart = await cartModel.update({
+                quantity: quantity,
+                total_amount: product.price * quantity
+            },{
+                where: {
+                    product_id : productID,
+                    traveler_id : userID
+                }
+            });
+
+            // 2. update quantity of product in vendor_essential table 
+            const product = await vendor_essentialModel.update({
+                quantity: product.quantity - quantity
+            },{
+                where: {
+                    id : productID
+                }
+            });
+
+        res.status(201).send({
+            message: "Product quantity updated successfully!",
+            cart: cart
+        });
+    }
+    } catch (err){
+        console.log(err);
+        res.status(500).send({
+            message: "Server Error!"
+        });
+    }
+}
+
+//remove from cart '$baseUrl/vendor/removeFromCart/:id'
+async function removeFromCart(req, res){
+    try{
+        const productID = req.params.id;
+        const userID = req.user.userID;
+        const removeProduct = await cartModel.findOne({
+            where: {
+                id : productID
+            }
+        })
+
+        // 1. remove product from cart 
+        const cart = await cartModel.destroy({
+            where: {
+                product_id : productID,
+                traveler_id : userID
+            }
+        });
+        // 2. update quantity of product in vendor_essential table (accordding to the quantity in cart)
+        const product = await vendor_essentialModel.update({
+            quantity: product.quantity + removeProduct.quantity
+        },{
+            where: {
+                id : productID
+            }
+        });
+
+        res.status(201).send({
+            message: "Product removed from cart successfully!",
+            cart: cart
+        });
+        
+    } catch (err){
+        console.log(err);
+        res.status(500).send({
+            message: "Server Error!"
+        });
+    }
+}
+
+
 
 //checkout '$baseUrl/vendor/checkout'
 
